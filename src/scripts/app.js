9ce95312c6f55e67f2e71c30f5fff68a4cec4609
bohdandidenko <bohdandidenko@yelk.com.ua>
2022-12-18 18:49:48 +0200

    feature(header mobile): added mobile menu;

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,17 @@
-(function () {
-  // write javascript here
-})();
+
+function initToggleMobMenu() {
+  const header = document.querySelector('.header');
+  const btn = header?.querySelector('.header__btn-menu');
+
+  if (!btn) return;
+
+  btn.addEventListener('click', () => {
+    document.body.classList.toggle('no-scroll');
+    header.classList.toggle('active');
+    btn.classList.toggle('active');
+  })
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  initToggleMobMenu();
+});
